bd2f0d5f3245a07df4129a2a7ecb9dbb8b8035c4
Jeeva Joslin <108604763+JEEVA-JOSLIN@users.noreply.github.com>
2024-12-08 19:24:10 +0530

    Update imagproc.py

--- imagproc.py
+++ imagproc.py
@@ -1,7 +1,6 @@
 import numpy as np
 import cv2
 
-
 def normalizeMeanVariance(in_img, mean=(0.485, 0.456, 0.406), variance=(0.229, 0.224, 0.225)):
     # should be RGB order
     img = in_img.copy().astype(np.float32)
