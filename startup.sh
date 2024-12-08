e95b823dfa63a5a282cfcc8cabc65946dc625344
Jeeva Joslin <108604763+JEEVA-JOSLIN@users.noreply.github.com>
2024-12-08 13:00:20 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -4,4 +4,7 @@ apt-get install -y build-essential
 apt-get install -y python3-dev
 apt-get install -y libjpeg-dev
 apt-get install -y libmupdf-dev mupdf-tools
+apt-get install -y tesseract-ocr 
+apt-get install -y libtesseract-dev 
+apt-get install -y poppler-utils 
 gunicorn --workers 3 --bind 0.0.0.0:8000 app:app --log-level debug
