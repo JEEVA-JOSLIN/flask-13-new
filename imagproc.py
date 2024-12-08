ce7088bc059578b9f77186c4df8a11056278e012
Jeeva Joslin <108604763+JEEVA-JOSLIN@users.noreply.github.com>
2024-12-08 19:08:14 +0530

    Update imagproc.py

--- imagproc.py
+++ imagproc.py
@@ -1,15 +1,6 @@
 import numpy as np
-from skimage import io
 import cv2
 
-def loadImage(img_file):
-    img = io.imread(img_file)           # RGB order
-    if img.shape[0] == 2: img = img[0]
-    if len(img.shape) == 2 : img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
-    if img.shape[2] == 4:   img = img[:,:,:3]
-    img = np.array(img)
-
-    return img
 
 def normalizeMeanVariance(in_img, mean=(0.485, 0.456, 0.406), variance=(0.229, 0.224, 0.225)):
     # should be RGB order
@@ -19,14 +10,7 @@ def normalizeMeanVariance(in_img, mean=(0.485, 0.456, 0.406), variance=(0.229, 0
     img /= np.array([variance[0] * 255.0, variance[1] * 255.0, variance[2] * 255.0], dtype=np.float32)
     return img
 
-def denormalizeMeanVariance(in_img, mean=(0.485, 0.456, 0.406), variance=(0.229, 0.224, 0.225)):
-    # should be RGB order
-    img = in_img.copy()
-    img *= variance
-    img += mean
-    img *= 255.0
-    img = np.clip(img, 0, 255).astype(np.uint8)
-    return img
+
 
 def resize_aspect_ratio(img, square_size, interpolation, mag_ratio=1):
     height, width, channel = img.shape
@@ -58,7 +42,4 @@ def resize_aspect_ratio(img, square_size, interpolation, mag_ratio=1):
 
     return resized, ratio, size_heatmap
 
-def cvt2HeatmapImg(img):
-    img = (np.clip(img, 0, 1) * 255).astype(np.uint8)
-    img = cv2.applyColorMap(img, cv2.COLORMAP_JET)
-    return img
+
